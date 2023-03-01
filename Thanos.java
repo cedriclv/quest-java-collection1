14a54091ced09a46cc576ff254243e0abffe72bf
CEDRIC LV <[cedriclevalegant@gmail.com](mailto:cedriclevalegant@gmail.com)>
2023-03-01 11:38:04 +0100

    lists

--- Thanos.java
+++ Thanos.java
@@ -4,24 +4,43 @@ public class Thanos {
 
     public static void main(String[] args) {
 
-        // TODO 1 : Create an empty heroes list aa
 
-        // TODO 2 : Add those heroes to the list
-        // name: Black Widow, age: 34
-        // name: Captain America, age: 100
-        // name: Vision, age: 3
-        // name: Iron Man, age: 48
-        // name: Scarlet Witch, age: 29
-        // name: Thor, age: 1500
-        // name: Hulk, age: 49
-        // name: Doctor Strange, age: 42
+        ArrayList<Hero> heroes = new ArrayList<>();
+        heroes.add(new Hero("Black Widow", 34));
+        heroes.add(new Hero("Captain America", 34));
+        heroes.add(new Hero("Vision", 34));
+        heroes.add(new Hero("Iron Man",48));
+        heroes.add(new Hero("Thor", 1500));
 
-        // TODO 3 : It's Thor birthday, now he's 1501
+        //GET INITIAL LIST
+        System.out.println("///Initial List :");
+        for (int i = 0; i < heroes.size(); i++) {
+            System.out.println(heroes.get(i).getName());
+        }
 
-        // TODO 4 : Shuffle the heroes list
+        // SHUFFLED THE LIST
+        ArrayList<Hero> heroesShuffled = new ArrayList<>();
+        while(heroes.size() > 0){
+            Random random = new Random();
+            int randomHero = random.nextInt(heroes.size());
+            heroesShuffled.add(heroes.get(randomHero));
+            heroes.remove(randomHero);
+        }
+        System.out.println("////Shuffled List :");
+        for (int i = 0; i < heroesShuffled.size(); i++) {
+            System.out.println(heroesShuffled.get(i).getName());
+        }
 
-        // TODO 5 : Keep only the half of the list
+        //CUT THE LIST IN 2
+        ArrayList<Hero> heroesCutHalf = new ArrayList<>();
+        for (int i = 0; i < heroesShuffled.size()/2; i++) {
+            heroesCutHalf.add(heroesShuffled.get(i));
+        }
+        
+        System.out.println("////Half List :");
+        for (int i = 0; i < heroesCutHalf.size(); i++) {
+            System.out.println(heroesCutHalf.get(i).getName());
+        }
 
-        // TODO 6 : Loop throught the list and display the name of the remaining heroes
     }
 }
